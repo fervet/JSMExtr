0239db14c14be2b5355ad14f8bcc66c1799945de
Antonio Jr <acdcjunior@gmail.com>
2016-09-19 12:47:56 -0300

    doc: include todos

--- src/index.js
+++ src/index.js
@@ -8,3 +8,14 @@ console.log("########################################");
 
 var tree = gumbo(htmlFileContents);
 console.log(JSON.stringify(tree));
+
+// parse html file
+// extract script tags with positional information
+// send script tags content to ES parser
+// parse ES
+
+// read JS file
+// send file content to ES parser
+// parse
+
+// input folder, do procedure above to each of folder
