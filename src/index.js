ad6e8360beae084f0ac80318c9ac94df411f3872
Antonio Jr <acdcjunior@gmail.com>
2016-09-22 23:56:06 -0300

    chore: update demo file used

--- src/index.js
+++ src/index.js
@@ -1,20 +1,10 @@
 const extractJavaScriptASTsFromHtmlFile = require("./parsing/extract-js-asts-from-html-file");
 
-const jsASTs = extractJavaScriptASTsFromHtmlFile('../spec/demo/demo.html', 'utf8');
-console.log(JSON.stringify(jsASTs));
+const jsASTs = extractJavaScriptASTsFromHtmlFile('../spec/demo/portal.html', 'utf8');
+// console.log(JSON.stringify(jsASTs));
+console.log(JSON.stringify(jsASTs, null, 4));
 console.log("####################");
 
-jsASTs.forEach(function (jsAST) {
-    console.log(JSON.stringify(jsAST, null, 4)+"\n\n");
-});
-
-// parse html file
-// extract script tags with positional information
-// send script tags content to ES parser
-// parse ES
-
-// read JS file
-// send file content to ES parser
-// parse
-
-// input folder, do procedure above to each of folder
+// jsASTs.forEach(function (jsAST) {
+//     console.log(JSON.stringify(jsAST, null, 4)+"\n\n");
+// });
