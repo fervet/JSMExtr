344bd6a93277dbac476f711922e8587d24d54388
Antonio Jr <acdcjunior@gmail.com>
2016-09-24 19:53:12 -0300

    test: add spec for extracting script tags from html file

---
+++ spec/parsing/html/extractScriptTagsFromHtmlFileSpec.js
@@ -0,0 +1,44 @@
+const extractScriptTagsFromHtmlFile = require("../../../src/parsing/html/extract-script-tags-from-html-file");
+
+describe("HTML", function () {
+
+    it("extract TAGs from HTML file", function () {
+        const htmlAST = extractScriptTagsFromHtmlFile('spec/demo/demo.html', 'utf8');
+
+        const expectedAST = [{
+            "originalTag": "<script>",
+            "originalEndTag": "</script>",
+            "nodeType": 1,
+            "nodeName": "script",
+            "tagName": "script",
+            "tagNamespace": "HTML",
+            "childNodes": [{
+                "nodeType": 3,
+                "nodeName": "#text",
+                "textContent": "console.log(\"one\");",
+                "startPos": {"line": 5, "column": 9, "offset": 84}
+            }],
+            "attributes": [],
+            "startPos": {"line": 5, "column": 1, "offset": 76},
+            "endPos": {"line": 5, "column": 28, "offset": 103}
+        }, {
+            "originalTag": "<script>",
+            "originalEndTag": "</script>",
+            "nodeType": 1,
+            "nodeName": "script",
+            "tagName": "script",
+            "tagNamespace": "HTML",
+            "childNodes": [{
+                "nodeType": 3,
+                "nodeName": "#text",
+                "textContent": "console.log(\"two\");",
+                "startPos": {"line": 6, "column": 9, "offset": 122}
+            }],
+            "attributes": [],
+            "startPos": {"line": 6, "column": 1, "offset": 114},
+            "endPos": {"line": 6, "column": 28, "offset": 141}
+        }];
+        expect(JSON.stringify(htmlAST, null, 4)).toEqual(JSON.stringify(expectedAST, null, 4));
+    });
+
+});
