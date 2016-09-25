e42ebac2c534adbf77fef1a4b87639fbfb526763
Antonio Jr <acdcjunior@gmail.com>
2016-09-25 02:11:00 -0300

    feat(clear-empty-properties): added utility function that clears empty properties from the js object (tree)

---
+++ spec/utils/clearEmptyPropertiesSpec.js
@@ -0,0 +1,64 @@
+const clearEmptyProperties = require("../../src/utils/clearEmptyProperties");
+
+
+function assertClear(name, input, expected) {
+    it(name, function () {
+        let r = clearEmptyProperties(input);
+        expect(r).toEqual(expected);
+    });
+}
+
+//noinspection JSUnusedLocalSymbols
+function xassertClear() {}
+
+describe("clear", function () {
+
+    assertClear(
+        "simple",
+        {
+            jr: 1,
+            ddd: undefined,
+            x: [undefined, 1, null, 2, {}]
+        },
+        {jr: 1, x: [1, 2]}
+    );
+
+    assertClear(
+        "nested",
+        {
+            jr: 1,
+            ddd: {eee: undefined}
+        },
+        {jr: 1}
+    );
+
+    assertClear(
+        "2-level nested",
+        {
+            jr: 1,
+            ddd: {eee: {fff: undefined}}
+        },
+        {jr: 1}
+    );
+
+    assertClear(
+        "array more",
+        {
+            jr: 1,
+            ddd: undefined,
+            x: [1, 2, {aaa: undefined}]
+        },
+        {jr: 1, x: [1, 2]}
+    );
+
+    assertClear(
+        "full",
+        {
+            jr: 1,
+            ddd: undefined,
+            x: [undefined, {ddd: {eee: {fff: undefined}}, www: [[[[{}],{}],{}],{zzz: [1]},undefined, null]}]
+        },
+        {jr: 1, x: [{www: [{zzz: [1]}]}]}
+    );
+
+});
---
+++ src/utils/clearEmptyProperties.js
@@ -0,0 +1,68 @@
+module.exports = clearEmptyProperties;
+
+function toS(obj) {
+    return JSON.stringify(obj, (key, value) => {if (value === undefined) return 'undef'; else return value; })
+}
+
+function clearEmptyProperties(obj) {
+    const shouldBeCleared = [obj];
+    for (let i = 0; i < shouldBeCleared.length; i++) {
+        let toPickProperties = shouldBeCleared[i];
+
+        if (Array.isArray(toPickProperties)) {
+            cleanArray(toPickProperties);
+            shouldBeCleared.push(...toPickProperties.filter(isObject));
+        } else {
+            if (isObject(toPickProperties)) {
+                clearEmptyProps(toPickProperties);
+                Object.keys(toPickProperties).forEach(key => {
+                    let v = toPickProperties[key];
+                    if (isObject(v)) {
+                        shouldBeCleared.push(v);
+                    }
+                });
+            }
+        }
+    }
+    while (shouldBeCleared.length > 0) {
+        let v = shouldBeCleared.pop();
+        clearEmptyProps(v);
+        cleanArray(v);
+    }
+    return obj;
+}
+
+function clearEmptyProps(obj) {
+    if (Array.isArray(obj)) {
+        return;
+    }
+    Object.keys(obj).forEach(key => {
+        if (isEmptyValue(obj[key])) {
+            delete obj[key];
+        }
+    });
+}
+
+function cleanArray(array) {
+    if (!Array.isArray(array)) {
+        return;
+    }
+    for (let i = 0; i < array.length; i++) {
+        if (isEmptyValue(array[i])) {
+            array.splice(i, 1);
+            i--;
+        }
+    }
+    return isEmptyValue;
+}
+
+function isEmptyValue(value) {
+    return value === undefined ||
+        value === null ||
+        (isObject(value) && Object.keys(value).length === 0 && value.constructor === Object) || // === {}
+        (Array.isArray(value) && value.length === 0); // === [];
+}
+
+function isObject(v) {
+    return v !== null && typeof v === 'object';
+}
