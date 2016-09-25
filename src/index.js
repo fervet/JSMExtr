26b4e2197d849b26ae82da2ce82fd0fb759b363c
Antonio Jr <acdcjunior@gmail.com>
2016-09-25 02:52:10 -0300

    test(metrics): print metrics 'summary'

--- src/index.js
+++ src/index.js
@@ -3,8 +3,12 @@ const functionMetricsFromJsAST = require("./metrics/extract-function-metrics-fro
 
 const jsASTs = extractJavaScriptASTsFromHtmlFile('spec/demo/portal.html', 'utf8');
 // console.log(JSON.stringify(jsASTs));
-console.log(JSON.stringify(functionMetricsFromJsAST(jsASTs), null, 4));
-console.log("####################");
+const functionMetrics = functionMetricsFromJsAST(jsASTs);
+// console.log(JSON.stringify(functionMetrics, null, 4));
+console.log("##########################################################");
+console.log("##########################################################");
+console.log("##########################################################");
+console.log(JSON.stringify(functionMetrics, (k, v) => {if (k === 'detail') return undefined; return v; }, 4));
 
 // jsASTs.forEach(function (jsAST) {
 //     console.log(JSON.stringify(jsAST, null, 4)+"\n\n");
