6c4e3990fe926a88ec98bafd81652bd48c0ff09c
Antonio Jr <acdcjunior@gmail.com>
2016-09-25 19:23:20 -0300

    feat(extract): outputting to R readable format

--- src/index.js
+++ src/index.js
@@ -12,14 +12,48 @@ console.log("##########################################################");
 functionMetrics.forEach(metrics => {
     delete metrics.detail;
     if (metrics._type === 'FunctionDeclaration') {
-        metrics.function = `${metrics.fileLocation} ~ ${metrics.functionName}`;
+        metrics.functionName = `${metrics.fileLocation} ~ ${metrics.functionName}`;
         delete metrics.fileLocation;
-        delete metrics.functionName;
     }
 });
 
+let toPrint = {
+    nomeFuncao: [],
+    declarationStmtCount: [],
+    executableStmtCount: [],
+    returnStmtCount: [],
+    parametersCount: [],
+    callExpressionCount: []
+};
+functionMetrics.forEach(f => {
+    if (f._type === 'FunctionDeclaration') {
+        toPrint.nomeFuncao.push(f.functionName);
+        toPrint.declarationStmtCount.push(f.metrics.declarationStmtCount || 0);
+        toPrint.executableStmtCount.push(f.metrics.executableStmtCount || 0);
+        toPrint.returnStmtCount.push(f.metrics.returnStmtCount || 0);
+        toPrint.parametersCount.push(f.metrics.parametersCount || 0);
+        toPrint.callExpressionCount.push(f.metrics.callExpressionCount || 0);
+    }
+});
+
+let out = `
+js <-
+structure(
+	list(
+		nomeFuncao = c("${toPrint.nomeFuncao.join('", "')}"),
+		declarationStmtCount = c(${toPrint.declarationStmtCount.join(", ")}),
+		executableStmtCount = c(${toPrint.executableStmtCount.join(", ")}),
+		returnStmtCount = c(${toPrint.returnStmtCount.join(", ")}),
+		parametersCount = c(${toPrint.parametersCount.join(", ")}),
+		callExpressionCount = c(${toPrint.callExpressionCount.join(", ")})
+	),
+	.Names = c("nomeFuncao", "declarationStmtCount", "executableStmtCount", "returnStmtCount", "parametersCount", "callExpressionCount"),
+	class = "data.frame",
+	row.names = c(NA, ${toPrint.nomeFuncao.length}L)
+)`;
+
 console.log(JSON.stringify(functionMetrics, null, 4));
 
-// jsASTs.forEach(function (jsAST) {
-//     console.log(JSON.stringify(jsAST, null, 4)+"\n\n");
-// });
+console.log("#####################################");
+console.log("#####################################");
+console.log(out);
