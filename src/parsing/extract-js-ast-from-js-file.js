fcccbc0276ff6903879c08814c2d6401b799d2bc
Antonio Jr <acdcjunior@gmail.com>
2016-10-03 21:51:58 -0300

    refactor(parsing): have extractors use new readFile() function

--- src/parsing/extract-js-ast-from-js-file.js
+++ src/parsing/extract-js-ast-from-js-file.js
@@ -1,11 +1,11 @@
-const fs = require('fs');
+const readFile = require("../utils/read-file");
 const parseJavaScriptString = require("./js/parse-js-string-to-ast");
 
 /**
  * Given a JavaScript file, returns one JS-AST for the entire code.
  */
 module.exports = function (fileName, fileEncoding) {
-    let jsFileContents = fs.readFileSync(fileName, fileEncoding);
+    let jsFileContents = readFile(fileName, fileEncoding);
     const jsAST = parseJavaScriptString(jsFileContents);
     jsAST.fileLocation = fileName;
     return jsAST;
--- src/parsing/html/extract-script-tags-from-html-file.js
+++ src/parsing/html/extract-script-tags-from-html-file.js
@@ -1,12 +1,13 @@
-const path = require('path');
-const fs = require('fs');
+const readFile = require("../../utils/read-file");
 const gumbo = require("gumbo-parser");
 
 /**
  * Given an HTML file, returns its list of script tags (in HTML-AST format).
+ *
+ * @return {Array}
  */
 module.exports = function (fileName, fileEncoding) {
-    let htmlFileContents = fs.readFileSync(fileName, fileEncoding);
+    let htmlFileContents = readFile(fileName, fileEncoding);
     let ast = gumbo(htmlFileContents);
     return extractScriptTags(ast.root);
 };
