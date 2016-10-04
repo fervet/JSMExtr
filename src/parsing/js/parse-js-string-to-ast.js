78005b91d40655fb5ec42cdc14def538f8bf0783
Antonio Jr <acdcjunior@gmail.com>
2016-10-04 13:37:47 -0300

    feat(parsing): deal with jsf EL expressions when parsing JS

--- src/parsing/js/parse-js-string-to-ast.js
+++ src/parsing/js/parse-js-string-to-ast.js
@@ -4,5 +4,27 @@ const esprima = require('esprima');
  * Given a string containing JavaScript source code, returns its JS-AST representation.
  */
 module.exports = function (javaScriptString) {
-    return esprima.parse(javaScriptString, {loc: true});
+    let javaScriptFixedString = javaScriptString;
+    if (javaScriptString.match(JSF_EL)) {
+        javaScriptFixedString = javaScriptString.replace(JSF_EL, replaceJSFELExpression);
+    }
+    return esprima.parse(javaScriptFixedString, {loc: true});
 };
+
+const JSF_EL = /#\{([^}]*)}/g;
+
+// no UT
+//noinspection JSUnusedLocalSymbols
+function replaceJSFELExpression(match, capture) {
+    if (capture.match(/^not empty/)) {
+        return true;
+    }
+    if (capture.match(/^rich:component\('[\w:.]+'\)/)) {
+        return "component";
+    }
+    if (capture.match(/^rich:clientId\('[\w:.]+'\)/)) {
+        return "clientId";
+    }
+    // console.log("#@MATCH: "+match);
+    return capture;
+}
