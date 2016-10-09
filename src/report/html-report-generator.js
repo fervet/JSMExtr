83dceb3c6626c09966e0f7b9310e9c56c0dfdff0
Antonio Jr <acdcjunior@gmail.com>
2016-10-09 04:08:18 -0300

    refactor(report): extract whole code into a parameterized function

--- src/report/html-report-generator.js
+++ src/report/html-report-generator.js
@@ -3,85 +3,103 @@ const Metrics = require('../metrics/Metrics');
 const Converter = require("csvtojson").Converter;
 const converter = new Converter({});
 
-const csvFile = "../../r/allClusters.csv";
+function generateHtmlReportFromCsv(csvFile, reportHtmlFile) {
+    console.info(`Generating HTML report from ${csvFile}...`);
+    converter.fromFile(csvFile, (err, allFunctionMetrics) => {
 
-converter.fromFile(csvFile, (err, allFunctionMetrics) => {
+        let clusters = {};
 
-    let clusters = {};
+        allFunctionMetrics.forEach(thisFunctionMetrics => {
+            //noinspection JSUnresolvedVariable
+            if (!clusters[thisFunctionMetrics.cluster]) {
+                //noinspection JSUnresolvedVariable
+                clusters[thisFunctionMetrics.cluster] = [];
+            }
+            //noinspection JSUnresolvedVariable
+            clusters[thisFunctionMetrics.cluster].push(thisFunctionMetrics);
+        });
 
-    allFunctionMetrics.forEach(thisFunctionMetrics => {
-        if (!clusters[thisFunctionMetrics.cluster]) {
-            clusters[thisFunctionMetrics.cluster] = [];
-        }
-        clusters[thisFunctionMetrics.cluster].push(thisFunctionMetrics);
-    });
+        let clusterTOC = "<ul>";
 
-    let clusterTOC = "<ul>";
-
-    let allTables = [];
-    Object.keys(clusters).forEach(clusterNumber => {
-        let functionsAtThisCluster = clusters[clusterNumber];
-        if (functionsAtThisCluster.length > 1) {
-            clusterTOC += `
+        let allTables = [];
+        Object.keys(clusters).forEach(clusterNumber => {
+            let functionsAtThisCluster = clusters[clusterNumber];
+            if (functionsAtThisCluster.length > 1) {
+                clusterTOC += `
                 <li>
                     <a href="#cluster-${clusterNumber}">
                     Cluster #${clusterNumber} (${functionsAtThisCluster.length} functions)
                     </a>
                 </li>
             `;
-            allTables.push(`
+                allTables.push(`
                  <h1 id="cluster-${clusterNumber}">Cluster #${clusterNumber}</h1>
                 ${createTableForFunctions(functionsAtThisCluster)}
             `);
-        }
+            }
+        });
+        clusterTOC += "</ul>";
+
+        writeClustersToHtmlFile(clusterTOC, allTables);
+        console.info("HTML Report generated.");
     });
-    clusterTOC += "</ul>";
 
-    writeClustersToHtmlFile(clusterTOC, allTables);
-});
-
-function writeClustersToHtmlFile(clusterTOC, tables) {
-    let htmlReport = `
+    function writeClustersToHtmlFile(clusterTOC, tables) {
+        let htmlReport = `
     <html>
     <head>
         <title>Function Clones Report</title>
         <style>table, tr, td, th { border: 1px solid black; border-collapse: collapse; }</style>
     </head>
     <body>
-    <h1>Function Clones Report</h1>
+    <h1><a href="https://github.com/acdcjunior/JSMExtr">JSMExtr</a> - Function Clones Report</h1>
     ${clusterTOC}
     <hr>
     ${tables.join("<br>")}
     </body>
     </html>
     `;
-    fs.writeFileSync("../../r/FunctionClones.html", htmlReport);
-}
+        console.info(`Writing HTML report to ${reportHtmlFile}...`);
+        fs.writeFileSync(reportHtmlFile, htmlReport);
+    }
 
-function createTableForFunctions(functionMetricsArray) {
-    let allRows = [];
-    functionMetricsArray.forEach(thisFunctionMetrics => {
-        let thisRow = `<tr>
+    function calculateFunctionLocs(functionFileLocation) {
+        return 1;
+    }
+
+    function createTableForFunctions(functionMetricsArray) {
+        let allRows = [];
+        const MAX_FILE_NAME = 60;
+        functionMetricsArray.forEach(thisFunctionMetrics => {
+            let thisRow = `<tr>
             <td>${thisFunctionMetrics["field1"]}</td>
             <td>${thisFunctionMetrics["functionName"]}</td>
             <td>
                 <a href="${thisFunctionMetrics["fileLocation"]}">
-                    ${thisFunctionMetrics["fileLocation"]}
+                    ${thisFunctionMetrics["fileLocation"].length > MAX_FILE_NAME ? "..." : ""
+                }${thisFunctionMetrics["fileLocation"].slice(-MAX_FILE_NAME)}
                 </a>
             </td>
+            <td>${calculateFunctionLocs(thisFunctionMetrics["fileLocation"])}</td>
         `;
-        Metrics.forEachTrackedMetric(trackedMetric => {
-            thisRow += `<td>${thisFunctionMetrics[trackedMetric]}</td>`;
+            Metrics.forEachTrackedMetric(trackedMetric => {
+                thisRow += `<td>${thisFunctionMetrics[trackedMetric]}</td>`;
+            });
+            thisRow += `</tr>`;
+            allRows.push(thisRow);
         });
-        thisRow += `</tr>`;
-        allRows.push(thisRow);
-    });
 
-    let headers = `<tr><th>#</th><th>Function Name</th><th>File Location</th>`;
-    Metrics.forEachTrackedMetric(trackedMetric => {
-        headers += `<th>${trackedMetric}</th>`;
-    });
-    headers += `</tr>`;
+        let headers = `<tr><th>#</th><th>Function Name</th><th>File Location</th><th>LOCs</th>`;
+        Metrics.forEachTrackedMetric(trackedMetric => {
+            headers += `<th>${trackedMetric}</th>`;
+        });
+        headers += `</tr>`;
 
-    return `<table>${headers}${allRows.join("")}</table>`;
+        return `<table>${headers}${allRows.join("")}</table>`;
+    }
 }
+
+const csvFile = "../../r/allClusters.csv";
+const reportHtmlFile = "../../r/JSMExtr-FunctionClonesReport.html";
+
+generateHtmlReportFromCsv(csvFile, reportHtmlFile);
